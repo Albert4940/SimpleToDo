06d56702a7b0ac8e8297a5d04c39f7101a6b4cee
Albert-Ubun <dorcealbertmary@gmail.com>
2019-06-26 17:38:25 -0400

    I declare an ArrayList, ListView and ArrayAdapter

--- app/src/main/java/com/albert/simpletodo/MainActivity.java
+++ app/src/main/java/com/albert/simpletodo/MainActivity.java
@@ -2,12 +2,20 @@ package com.albert.simpletodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvitems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
